79227419f21b2c21bcea85ee02365a764d894a56
Jose Fabio Bermudez <fbermudez86@gmail.com>
2016-08-16 22:12:57 -0600

    Module: Async in Redux
    Added webpack-dashboard
    TODO:
    -Still need to add the component to display the courses

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -6,22 +6,8 @@ class CoursesPage extends React.Component {
   constructor(props, context)
   {
     super(props, context);
-    this.state = {
-      course: { title: ""}
-    };
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
   }
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({ course: course });
-  }
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
-    //Clear the input after saving the title
-    this.setState({course: {title: ''}});
-  }
+
   courseRow(course, index)
   {
     return <div key={index}>{course.title}</div>;
@@ -31,16 +17,6 @@ class CoursesPage extends React.Component {
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Add Courses</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title}/>
-        <input
-          type="submit"
-          value="save"
-          onClick={this.onClickSave}
-        />
       </div>
     );
   }
--- tools/srcServer.js
+++ tools/srcServer.js
@@ -3,6 +3,8 @@ import webpack from 'webpack';
 import path from 'path';
 import config from '../webpack.config.dev';
 import open from 'open';
+import Dashboard from 'webpack-dashboard';
+import DashboardPlugin from 'webpack-dashboard/plugin';
 
 /* eslint-disable no-console */
 
@@ -10,12 +12,19 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
+const dashboard = new Dashboard();
+
+compiler.apply(new DashboardPlugin(dashboard.setData));
+
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
+  quiet: true,
   publicPath: config.output.publicPath
 }));
 
-app.use(require('webpack-hot-middleware')(compiler));
+app.use(require('webpack-hot-middleware')(compiler,
+  { log: () => {}}
+));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join( __dirname, '../src/index.html'));
